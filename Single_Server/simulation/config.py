b27f6cc7e84b0a472b1f1de0410de861dc7e6de0
frogcovenant <judithvelram@gmail.com>
2021-06-18 01:54:50 -0500

    added a new configuration close to what was asked for

--- Single_Server/simulation/config.py
+++ Single_Server/simulation/config.py
@@ -1,9 +1,9 @@
-RANDOM_SEED = 42
+RANDOM_SEED = 34
 NEW_CUSTOMERS = 25              # Total number of customers
-INTERVAL_CUSTOMERS = 4.0        # Generate new customers roughly every x seconds
+INTERVAL_CUSTOMERS = 4        # Generate new customers roughly every x seconds
 MIN_PATIENCE = 2                # Min. gen_customer patience
-MAX_PATIENCE = 6                # Max. gen_customer patience
-CAPACITY = 2                    # The capacity of our resource
+MAX_PATIENCE = 5                # Max. gen_customer patience
+CAPACITY = 2                   # The capacity of our resource
 TIME_IN_BANK = 10.0             # The max time an operation can take
 REPORT_STEP_BY_STEP = True     # Flag to report step by step events
 REPORT_QUEUE = True            # Flag to report step by step the state of the queue
